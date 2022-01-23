9fae64dc156074fe51299a2da5e3e4b364e8cdb2
Daniel Atonge <elamboatonge@gmail.com>
2022-01-23 17:39:05 +0300

    connects to aws amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "unstoppable7dfbef1e": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
